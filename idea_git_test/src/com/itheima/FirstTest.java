1410de74951aa89ecc3fcb9ea4c523e3cc44eead
LB <liubinstruggle@163.com>
2019-04-29 20:47:18 +0800

    修改

--- idea_git_test/src/com/itheima/FirstTest.java
+++ idea_git_test/src/com/itheima/FirstTest.java
@@ -12,11 +12,27 @@ public class FirstTest {
         System.out.println("add方法");
     }
 
+    /**
+     * 删除
+     */
     public void del(){
         System.out.println("删除方法");
     }
 
+    /**
+     * 修改方法
+     */
+    public void update(){
+        System.out.println("修改方法");
+    }
+
+    /**
+     * 查找
+     */
     public void find(){
         System.out.println("查询方法");
     }
+
+
+
 }
